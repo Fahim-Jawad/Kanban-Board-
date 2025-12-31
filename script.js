48322e2cf122b4f09d10fcda39f8c4754bfbf1b5
Fahim-Jawad <fahimjawad5@gmail.com>
2025-12-31 07:10:12 +0600

    making elements draggable and making the function of draging evevents on columns

---
+++ script.js
@@ -0,0 +1,31 @@
+const todo = document.querySelector("#to-do");
+const progress = document.querySelector("#progress");
+const done = document.querySelector("#done");
+
+const tasks = document.querySelectorAll(".task");
+
+tasks.forEach((task) => {
+  task.addEventListener("drag", (e) => {});
+});
+
+function addDragEventsOnColumn(column) {
+  column.addEventListener("dragenter", (e) => {
+    e.preventDefault();
+    column.classList.add("hover-over");
+  });
+  column.addEventListener("dragleave", (e) => {
+    e.preventDefault();
+    column.classList.remove("hover-over");
+  });
+  column.addEventListener("dragover", (e) => {
+    e.preventDefault();
+  });
+  column.addEventListener("drop", (e) => {
+    e.preventDefault();
+    console.log("dropped", e);
+  });
+}
+
+addDragEventsOnColumn(todo);
+addDragEventsOnColumn(progress);
+addDragEventsOnColumn(done);
